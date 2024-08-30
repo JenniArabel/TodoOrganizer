159142563f47eba0985fa0dd222a9dfbab30c6f1
Jennifer Arabel <jennifer.arabel@gmail.com>
2024-08-30 11:31:44 -0300

    Modify TodoItem.js to import delete icon and completed icon

--- src/assets/TodoItem.js
+++ src/assets/TodoItem.js
@@ -1,25 +1,25 @@
-import './TodoItem.css'
+import React from "react";
+import { FaCheck } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import "./TodoItem.css";
 
-function TodoItem (props) {
+function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span 
-        className= {`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+      <span
+        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
         onClick={props.onComplete}
-      > 
-      V 
+      >
+        <FaCheck />
       </span>
-        
-      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}> {props.text} </p>
-        
-      <span 
-        className='Icon Icon-delete'
-        onClick={props.onDelete}
-      > 
-      X 
+
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`} > {" "} {props.text} {" "} </p>
+
+      <span className="Icon Icon-delete" onClick={props.onDelete}>
+        <ImCross />
       </span>
     </li>
   );
 }
-  
+
 export { TodoItem };
