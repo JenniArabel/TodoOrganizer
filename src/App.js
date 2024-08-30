cefba80776fcd74e73cafe5abf169362bda3f13c
Jennifer Arabel <jennifer.arabel@gmail.com>
2024-08-30 10:05:07 -0300

    Modify App.js and TodoCounter.js to add a message in case all todos are completed or when there are no todos created

--- src/App.js
+++ src/App.js
@@ -1,91 +1,92 @@
-import './App.css';
-import React from 'react';
-import { TodoCounter } from './assets/TodoCounter';
-import { TodoSearch } from './assets/TodoSearch';
-import { TodoList } from './assets/TodoList';
-import { TodoItem } from './assets/TodoItem';
-import { CreateTodoButton } from './assets/CreateTodoButton';
-
+import "./App.css";
+import React from "react";
+import { TodoCounter } from "./assets/TodoCounter";
+import { TodoSearch } from "./assets/TodoSearch";
+import { TodoList } from "./assets/TodoList";
+import { TodoItem } from "./assets/TodoItem";
+import { CreateTodoButton } from "./assets/CreateTodoButton";
 
 const defaultTodos = [
-  {text: 'Estudiar', completed: true},
-  {text: 'Hacer investigación', completed: false},
-  {text: 'Hacer TPs', completed: false},
-  {text: 'Clase estados derivados', completed: true}
+  { text: "Estudiar", completed: true },
+  { text: "Hacer investigación", completed: false },
+  { text: "Hacer TPs", completed: false },
+  { text: "Clase estados derivados", completed: true },
 ];
 
 function App() {
   //Estados del componente
   const [todos, setTodos] = React.useState(defaultTodos);
-  const [searchValue, setSearchValue] = React.useState(''); 
+  const [searchValue, setSearchValue] = React.useState("");
 
   //Variables o estados derivados
-  const completedTodos = todos.filter(todo => !!todo.completed).length; 
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
 
   //Funcionalidad de búsqueda
-  const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLocaleLowerCase(); //.toLocaleLowerCase incluye acentos
-      
-      const searchText = searchValue.toLocaleLowerCase();
-      
-      return todoText.includes(searchText);
-    }
-  );
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLocaleLowerCase(); //.toLocaleLowerCase incluye acentos
+
+    const searchText = searchValue.toLocaleLowerCase();
+
+    return todoText.includes(searchText);
+  });
 
   //Fx para completar una tarea
-  const completeTodo = (text) => { //Recibe un texto
+  const completeTodo = (text) => {
+    //Recibe un texto
     const newTodos = [...todos]; //Hace una copia del estado actual de las tareas
-    
-    const todoIndex = newTodos.findIndex( //Encuentra el indice de la tarea
+
+    const todoIndex = newTodos.findIndex(
+      //Encuentra el indice de la tarea
       (todo) => todo.text == text
     );
-   
+
     newTodos[todoIndex].completed = true; //Cambia la propiedad completed a true
-    
+
     setTodos(newTodos); //Actualiza el estado de todos con la lista modificada
-  }
+  };
 
   //Fx para eliminar una tarea
   const deleteTodo = (text) => {
     const newTodos = [...todos];
-    
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
-    );
-    
+
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+
     newTodos.splice(todoIndex, 1); //Elimina la tarea de la lista usando splice
-    
+
     setTodos(newTodos);
+  };
+
+  //Mensaje basado en los estados de las tareas
+  let message = '';
+  if (totalTodos === 0) {
+    message = 'Crea tu primer tarea';
+  } else if (completedTodos === totalTodos) {
+    message = "¡Felicitaciones! No hay tareas pendientes"
   }
-
-  //Mensaje en caso de todas las tareas están completadas
-  const allCompletedMessage = completedTodos === totalTodos && totalTodos > 0 ? "¡Felicitaciones! No hay tareas pendientes" : "";
-
+  
   //Renderizado
   return (
-    <div className='App'>
-      <TodoCounter 
+    <div className="App">
+      <TodoCounter
         completed={completedTodos}
         total={totalTodos}
-        message={allCompletedMessage}
+        message={message}
       />
 
       <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
+        searchValue={searchValue} 
+        setSearchValue={setSearchValue} 
       />
 
       <TodoList>
-        {searchedTodos.map( todo => (
-          <TodoItem 
+        {searchedTodos.map((todo) => (
+          <TodoItem
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
             onComplete={() => completeTodo(todo.text)}
             onDelete={() => deleteTodo(todo.text)}
-
           />
         ))}
       </TodoList>
--- src/assets/TodoCounter.js
+++ src/assets/TodoCounter.js
@@ -1,24 +1,23 @@
-import './TodoCounter.css'
+import "./TodoCounter.css";
 
-function TodoCounter({ completed, total, message }) { 
+function TodoCounter({ completed, total, message }) {
   return (
     <>
-        <h1 className='Title'> ToDo Organizer </h1>
+      <h1 className="Title"> ToDo Organizer </h1>
 
-        {message ? (
-          <h4 className='TodoCounter'> {message} </h4>
-        ) : (
-          <h4 className='TodoCounter'> 
-            Has completado 
-            <span> {completed} </span>  
-            de 
-            <span> {total} </span>  
-            tareas 
+      {message ? (
+        <h4 className="TodoCounter"> {message} </h4>
+      ) : (
+        <h4 className="TodoCounter">
+          Has completado
+          <span> {completed} </span>
+          de
+          <span> {total} </span>
+          tareas
         </h4>
-      )}  
+      )}
     </>
   );
 }
 
 export { TodoCounter };
-
